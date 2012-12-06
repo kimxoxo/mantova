ab25f9da0e66792b751369691b1425d7d305a8d5
kimxoxo <kimxoxo@aeiou.pt>
2012-12-06 19:38:32 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mantova::Application.config.secret_token = 'b821517354bf26e1c96ecc3ba84becd1345b42686d2e513add4e6fe416d59db3ad9c5e3e2e4075de8025a61d4aa4fdfe7f049fde6ff9a7086d8e3797d1e125b9'
